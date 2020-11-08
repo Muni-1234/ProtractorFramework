f12143bb43ede94e1312733ffdf518185a1f43aa
Muni-123 <munichandra.reddyk@gmail.com>
2020-11-08 13:30:13 +0100

    updated calculator.js

--- testcases/Calculator.js
+++ testcases/Calculator.js
@@ -19,4 +19,22 @@ describe('demo calculator tests', function () {
         browser.sleep(3000);
     })
 
+    it('substraction test', function () {
+
+        //With Page Object Model(POM) Approach:
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        browser.manage().window().maximize();
+        expect(browser.getCurrentUrl()).toBe('http://juliemr.github.io/protractor-demo/');
+        browser.sleep(2000);
+
+        homepage.enterfirstNumber('5');
+        homepage.entersecondNumber('5');
+        homepage.clickGo1();
+        browser.sleep(2000);
+        homepage.verifyResult('10')
+        browser.sleep(2000);
+
+    })
+
 })
